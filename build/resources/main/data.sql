f3b509a9c64d730cb0aea54c6e8ff9c3ac15b93c
dan <dabdullaev@miu.edu>
2021-08-08 22:52:08 -0500

    Change to PostgreSQL

--- build/resources/main/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tbl_notes(title, body, category) VALUES("note1", "note1 description", "cat1");
-INSERT INTO tbl_notes(title, body, category) VALUES("note2", "note2 description", "cat2");
